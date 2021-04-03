d1b4ac948504c5efdce0061424828881a7f349cc
Prasamsha Sharma <prasamshari@gmail.com>
2021-04-03 13:05:02 -0700

    first commit

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <h1>Free Pics from Unsplash </h1>
+      <p>React Hooks Mui</p>
+    </div>
+  );
+};
+
+export default Heading;
